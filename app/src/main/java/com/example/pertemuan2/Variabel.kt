e6160076032b2330a0a3049676de15e0e8b4fd64
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-10-13 13:18:55 +0700

    membuat function nilai tetap tidak bisa diubah dengan tipe data byte dan int

--- app/src/main/java/com/example/pertemuan2/Variabel.kt
+++ app/src/main/java/com/example/pertemuan2/Variabel.kt
@@ -1,2 +1,13 @@
 package com.example.pertemuan2
 
+fun main() {
+    // Byte: Tipe data untuk bilangan bulat kecil, rentang -128 hingga 127.
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Int: Tipe data untuk bilangan bulat, rentang -2.147.483.648 hingga 2.147.483.647.
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+
+}
