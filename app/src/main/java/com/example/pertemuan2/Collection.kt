0735634e262bbf8f384891fc428af1dc93be4494
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-10-13 14:03:30 +0700

    Membuat fungsi ContohSet dan implementasi operasi dasar pada Set: menampilkan Set Read-Only, menambahkan elemen pada Set Mutable, dan membuat Set Read-Only dari Set Mutable.

--- app/src/main/java/com/example/pertemuan2/Collection.kt
+++ app/src/main/java/com/example/pertemuan2/Collection.kt
@@ -32,6 +32,31 @@ fun ContohList(){
 }
 
 
+fun ContohSet(){
+    println()
+    println("=== Set ===")
+
+
+    // Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+
+    // Set Mutable
+    val mutableShapes: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(mutableShapes)
+
+
+    // Menambahkan data kedalam Set Mutable
+    mutableShapes.add("Rectangle")
+    println(mutableShapes)
+
+
+    // Set Read-Only
+    val shapeLocked: Set<String> = mutableShapes
+    println(shapeLocked)
+}
+
 
 
 
