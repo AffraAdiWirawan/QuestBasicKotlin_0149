73966ee69777754110140bd28b4e3d7f4abb622d
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-10-13 14:20:53 +0700

    Menggunakan If-Else untuk memeriksa angka positif atau negatif

--- app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
+++ app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
@@ -3,6 +3,16 @@ package com.example.pertemuan2
 fun ConditionalStatement(){
     println("=== Conditional Expression ===")
 
+// If-Else
+    val number = 10
+    if (number > 0){
+        println("Positive number")
+    } else {
+        println("Negative number")
+    }
+
+
+    
 
 }
 
