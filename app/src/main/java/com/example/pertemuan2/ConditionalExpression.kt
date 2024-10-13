d96fc1f65f6d391a0eaebfea91baf103111372fa
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-10-13 14:22:08 +0700

    Menggunakan When untuk mencetak nama hari berdasarkan nomor

--- app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
+++ app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
@@ -11,8 +11,21 @@ fun ConditionalStatement(){
         println("Negative number")
     }
 
+// When
+    val day = 1
+    when (day){
+        1 -> println ("Minggu")
+        2 -> println ("Senin")
+        3 -> println ("Selasa")
+        4 -> println ("Rabu")
+        5 -> println ("Kamis")
+        6 -> println ("Jumat")
+        7 -> println ("Sabtu")
+        else -> println ("Invalid")
+    }
 
-    
+
+
 
 }
 
