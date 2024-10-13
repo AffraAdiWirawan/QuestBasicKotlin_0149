5cbdebe9cba11ec5cb21c507a99f6914e2d754d1
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-10-13 14:17:00 +0700

    Mendefinisikan variabel non-nullable dan nullable.

--- app/src/main/java/com/example/pertemuan2/NullSafety.kt
+++ app/src/main/java/com/example/pertemuan2/NullSafety.kt
@@ -4,7 +4,15 @@ import org.jetbrains.annotations.NotNull
 
 
 fun NullSafety(){
+    var neverNull: String = "This can't be null"
 
+
+    // Nullable has nullable string type
+    var nullable: String? = "You can a null here"
+    nullable = null
+
+
+   
 }
 
 
